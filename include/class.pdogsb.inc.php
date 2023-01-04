e6ae5d5a2eb2275b7effd9724aa37fc3f01e83fa
Bryan <barbierbryan42@gmail.com>
2023-01-04 12:05:43 +0100

    Commit

--- include/class.pdogsb.inc.php
+++ include/class.pdogsb.inc.php
@@ -17,9 +17,9 @@
 
 class PdoGsb{   		
       	private static $serveur='mysql:host=localhost';
-      	private static $bdd='dbname=gsb_frais';   		
-      	private static $user='gsb_user' ;    		
-      	private static $mdp='password' ;	
+      	private static $bdd='dbname=gsb';   		
+      	private static $user='root' ;    		
+      	private static $mdp='M!Y7!54$44bSxC&$' ;	
 		private static $monPdo;
 		private static $monPdoGsb=null;
 /**
@@ -231,8 +231,8 @@ class PdoGsb{
 */
 	public function creeNouveauFraisHorsForfait($idVisiteur,$mois,$libelle,$date,$montant){
 		$dateFr = dateFrancaisVersAnglais($date);
-		$req = "insert into LigneFraisHorsForfait 
-		values('','$idVisiteur','$mois','$libelle','$dateFr','$montant')";
+		$req = "insert into LigneFraisHorsForfait(idVisiteur,mois,libelle,date,montant) 
+		values('$idVisiteur','$mois','$libelle','$dateFr','$montant')";
 		PdoGsb::$monPdo->exec($req);
 	}
 /**
